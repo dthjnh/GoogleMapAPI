066b2a660194af0b28cf6fc5b318c22ed7ed8fbe
ken <lbj200457@gmail.com>
2024-11-27 15:52:06 +0700

    a

--- app/src/main/java/com/example/googleapi/MapsActivity.java
+++ app/src/main/java/com/example/googleapi/MapsActivity.java
@@ -5,6 +5,8 @@ import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.FragmentActivity;
 import android.Manifest;
 
+import android.content.pm.PackageManager;
+import android.graphics.Color;
 import android.location.Location;
 import android.os.Bundle;
 import android.view.View;
@@ -20,10 +22,13 @@ import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.Circle;
+import com.google.android.gms.maps.model.CircleOptions;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.example.googleapi.databinding.ActivityMapsBinding;
+import com.google.android.gms.tasks.OnSuccessListener;
 
 import kotlin.Suppress;
 
@@ -68,14 +73,18 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
         // Add a marker in Sydney and move the camera
         LatLng rmit = new LatLng(10.73, 106.69);
-        mMap.addMarker(new MarkerOptions().position(rmit).title("Marker in RMIT"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(rmit));
-        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(rmit, 15));
+        mMap.addMarker(new MarkerOptions().position(rmit).title("Marker in RMIT"));//add marker
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(rmit));//move camera to the marker
+        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(rmit, 15));//zoom level 1-20
+        mMap.getUiSettings().setZoomControlsEnabled(true);//zoom in zoom out
+
+
 
         mMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
             @Override
             public void onMapClick(LatLng latLng) {
                 mMap.addMarker(new MarkerOptions().position(latLng).icon(BitmapDescriptorFactory.defaultMarker()));
+                drawCircle(latLng);
             }
         });
 
@@ -95,8 +104,39 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     private void requestPermission() {
             ActivityCompat.requestPermissions(MapsActivity.this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION},MY_PERMISSION_REQUEST_CODE);
         }
-    public void getPosition(View view){
+    public void getPosition(View view) {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        client.getLastLocation().addOnSuccessListener(this, new OnSuccessListener<Location>() {
+            @Override
+            public void onSuccess(Location location) {
+                if (location != null) {
+                    // Get the current location and update the map
+                    LatLng currentLatLng = new LatLng(location.getLatitude(), location.getLongitude());
+
+                    mMap.addMarker(new MarkerOptions().position(currentLatLng).title("Current Location"));
+                    mMap.moveCamera(CameraUpdateFactory.newLatLng(currentLatLng));
+                    mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(currentLatLng, 15));
+
+
+                    String message = "Current Location: " + location.getLatitude() + ", " + location.getLongitude();
+                    Toast.makeText(MapsActivity.this, message, Toast.LENGTH_SHORT).show();
+                } else {
+                    // Show an error message if location is null
+                    Toast.makeText(MapsActivity.this, "Unable to get current location", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
+
 
     @Suppress(names = "MissingPermission")
     private void startLocationUpdate() {
@@ -118,5 +158,10 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mMap.addMarker(new MarkerOptions().position(latLng).title("Current Location"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
         Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
+    }
+
+    private void drawCircle(LatLng latLng) {
+        CircleOptions circleOptions = new CircleOptions().center(latLng).radius(100).fillColor(Color.RED).strokeColor(Color.BLACK).strokeWidth(2);
+        mMap.addCircle(circleOptions);
     }
 }
